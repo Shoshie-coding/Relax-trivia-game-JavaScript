75260318bc9f3513750f34d26820596f84011f29
Cristina Onea <cristinaonea86@gmail.com>
2021-10-14 18:06:28 +0000

    Change google fonts

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,4 @@ const inputCategoryName = document.querySelector('#quiz-category-selector')
 const progressText = document.querrySelector('progress-text');
 const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('progress-bar-full');
+
