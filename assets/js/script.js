fa672071a2c3a99df880497a90d3c83da0cc0241
Cristina Onea <cristinaonea86@gmail.com>
2021-10-14 17:04:51 +0000

    Add const variables for question, category selector, score and progress bar

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const question = document.querySelector('#question');
+const inputCategoryName = document.querySelector('#quiz-category-selector')
+const progressText = document.querrySelector('progress-text');
+const scoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('progress-bar-full');
