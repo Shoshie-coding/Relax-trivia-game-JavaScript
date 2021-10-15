400eb5bb7d63dc25865c8cc04477ade7ed5dc9a5
Cristina Onea <cristinaonea86@gmail.com>
2021-10-15 11:50:19 +0000

    Add variables for game questions and answers and questions array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,21 @@
-const question = document.querySelector('#question');
+// Game variables
+
+const question = document.getElementById('question');
+const choices = Array.from(document.getElementsByClassName('choice-text'));
+
+let currentQuestion = ();
+let acceptAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions =[]
+
+let Questions = []
+
+
+
+
+
+
 const inputCategoryName = document.querySelector('#quiz-category-selector')
 const progressText = document.querrySelector('progress-text');
 const scoreText = document.querySelector('#score');
