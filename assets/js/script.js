c670ed9776d3a154bd6877da567cb9a51e308f65
Cristina Onea <cristinaonea86@gmail.com>
2021-10-15 19:18:08 +0000

    Add event listeners to choices and update the UI to reflect the questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,22 +3,92 @@
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
 
-let currentQuestion = ();
-let acceptAnswers = true;
+let currentQuestion = {};
+let acceptAnswers = false;
 let score = 0;
 let questionCounter = 0;
-let availableQuestions =[]
+let availableQuestions = [];
 
-let Questions = []
+let questions = [
+    {
+        question: "Which of these countries remained neutral during World War II?",
+        choice1: "United Kingdom",
+        choice2: "France",
+        choice3: "Italy",
+        choice4: "Switzerland",
+        answer: 4,
+    },
+     
+    {
+        question: "In the War of the Pacific (1879 - 1883), Bolivia lost its access to the Pacific Ocean after being defeated by which South American country?",
+        choice1: "Chile",
+        choice2: "Peru",
+        choice3: "Brazil",
+        choice4: "Argentina",
+        answer: 4,
+
+    },
+
+    {
+        question: " How do you write 'Hello World' in an alert box?",
+        choice1: "msgBox('Hello World');",
+        choice2: "alertBox('Hello World');",
+        choice3: "msg('Hello World');",
+        choice4: "alert('Hello World');",
+        answer: 4,
+    },
+];
+       
+// Constants
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 3;
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions]
+    getNewQuestion();
+};
+
+getNewQuestion = () => {
+    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        // Go to end page
+        return window.location.assign('/end.html');
+
+    }
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach((choice) => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+
+    });
+
+    availableQuestions.splice(QuestionIndex, 1);
+    acceptingAnswers = true;
+
+};
+
+choices.forEach((choice) => {
+    choice.addEventListener('click', (e) => {
+        if (!acceptAnswers) return;
+
+        acceptAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number'];
+        getNewQuestion();
+    });
+});
+
+startGame();
 
 
 
 
 
 
-const inputCategoryName = document.querySelector('#quiz-category-selector')
-const progressText = document.querrySelector('progress-text');
-const scoreText = document.querySelector('#score');
-const progressBarFull = document.querySelector('progress-bar-full');
 
 
