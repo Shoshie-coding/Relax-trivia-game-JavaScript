d2e6c93b96be36713be06c0c73bf14d04a846cbe
Cristina Onea <cristinaonea86@gmail.com>
2021-10-18 22:23:13 +0000

    Add questions to history category in script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,23 +32,44 @@ let questions = [{
 },
 
 {
-    question: "In the War of the Pacific (1879 - 1883), Bolivia lost its access to the Pacific Ocean after being defeated by which South American country?",
-    choice1: "Chile",
-    choice2: "Peru",
-    choice3: "Brazil",
-    choice4: "Argentina",
-    answer: 4,
+    question: "Which Apollo mission was the last one in NASA's Apollo program?",
+    choice1: "Apollo 17",
+    choice2: "Apollo 13",
+    choice3: "Apollo 11",
+    choice4: "Apollo 15",
+    answer: 1,
+},
+
+{
+    question: "Who was the first president of the United States?",
+    choice1: "James Madison",
+    choice2: "George Washington",
+    choice3: "Thomas Jefferson",
+    choice4: "James K. Polk",
+    answer: 2,
 
 },
 
 {
-    question: " How do you write 'Hello World' in an alert box?",
-    choice1: "msgBox('Hello World');",
-    choice2: "alertBox('Hello World');",
-    choice3: "msg('Hello World');",
-    choice4: "alert('Hello World');",
-    answer: 4,
+    question: "During the Roman Triumvirate of 42 BCE, what region of the Roman Republic was given to Lepidus?",
+    choice1: "Italia",
+    choice2: "Gallia",
+    choice3: "Hispania",
+    choice4: "Asia",
+    answer: 3,
+
 },
+
+{
+    question: "What was the name commonly given to the ancient trade routes that connected the East and West of Eurasia?",
+    choice1: "Spice Road",
+    choice2: "Clay Road",
+    choice3: "Salt Road",
+    choice4: "Silk Road",
+    answer: 4,
+
+},
+
 ];
 
 switch (myParam) {
